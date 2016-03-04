8b4f0b0cdb49f6ac6d903d61feaae871e4e0a616
Ivaylo Goranov <ivaylo.v.goranov@gmail.com>
2016-03-04 18:03:31 +0200

    Added ball movement.

--- Breakout/BreakoutMain.cs
+++ Breakout/BreakoutMain.cs
@@ -10,6 +10,15 @@ namespace Breakout
         private const int PaddleWidth = 7;
         private const char BallSymbol = '*';
 
+        private static int paddlePositionX = 18; //Paddle x-coordinate starting position; y-coordinate is always PlaygroundHeight - 1
+        //(i.e. bottom of the screen).
+        
+        private static int ballPositionX = paddlePositionX + 3; //Ball x-coordinate starting position.
+        private static int ballPositionY = PlaygroundHeight - 1; //Ball y-coordinate starting position.
+
+        //Valid predefined ball directions: 0 - up, 1 - up left, 2 - up right, 3 - down, 4 - down right, 5 - down left.
+        private static int ballDirection = 0;
+
         public static void Main()
         {
             Console.CursorVisible = false;
@@ -18,61 +27,148 @@ namespace Breakout
             Console.BufferHeight = Console.WindowHeight;
             Console.BufferWidth = Console.WindowWidth;
 
-            int playerPositionX = 18; //Paddle x-coordinate starting position; y-coordinate is always PlaygroundHeight - 1
-            //(i.e. bottom of the screen).
-            DrawPaddle(playerPositionX);
-
-            int ballPositionX = playerPositionX + 3; //Ball x-coordinate starting position.
-            int ballPositionY = PlaygroundHeight - 1; //Ball y-coordinate starting position.
-            DrawBall(ballPositionX, ballPositionY);
+            DrawPaddle();
+            DrawBall();
 
             while (true)
             {
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo pressedKey = Console.ReadKey();
-                    playerPositionX = ChangePaddlePosition(playerPositionX, pressedKey);
+                    ChangePaddlePosition(pressedKey);
                 }
 
+                ChangeBallPosition();
                 Console.Clear();
-                DrawPaddle(playerPositionX);
-                DrawBall(ballPositionX, ballPositionY);
+                DrawPaddle();
+                DrawBall();
                 Thread.Sleep(100);
             }
         }
 
-        private static int ChangePaddlePosition(int playerPositionX, ConsoleKeyInfo pressedKey)
+        private static void ChangeBallPosition()
+        {
+            switch (ballDirection)
+            {
+                case 0: //Up
+                    ballPositionY--;
+                    break;
+                case 1: //Up left
+                    ballPositionX--;
+                    ballPositionY--;
+                    break;
+                case 2: //Up right
+                    ballPositionX++;
+                    ballPositionY--;
+                    break;
+                case 3: //Down
+                    ballPositionY++;
+                    break;
+                case 4: //Down right
+                    ballPositionX++;
+                    ballPositionY++;
+                    break;
+                case 5: //Down left
+                    ballPositionX--;
+                    ballPositionY++;
+                    break;
+            }
+
+            if (ballPositionY == PlaygroundHeight - 1) //When the ball hits the floor or the paddle.
+            {
+                if ((ballPositionX >= paddlePositionX + 2) &&
+                    (ballPositionX <= paddlePositionX + 4)) // The middle 3 "_" symbols.
+                {
+                    ballDirection = 0; //Bouncing up.
+                }
+                else if ((ballPositionX >= paddlePositionX) &&
+                    (ballPositionX <= paddlePositionX + 1)) // The left 2 "_" symbols.
+                {
+                    ballDirection = 1; //Bouncing up and to the left.
+                }
+                else if ((ballPositionX >= paddlePositionX + 5) &&
+                    (ballPositionX <= paddlePositionX + 6)) // The right 2 "_" symbols.
+                {
+                    ballDirection = 2; //Bouncing up and to the right.
+                }
+                else
+                {
+                    Console.WriteLine("Game Over!"); //The ball did not land on the paddle.
+                    Environment.Exit(0); //Terminates the main thread, without any exception thrown, i.e. exits the program.
+                }
+            }
+
+            if (ballPositionY == 0) //When the ball hits the ceiling.
+            {
+                if (ballDirection == 0)
+                {
+                    ballDirection = 3; //From upward direction the ball bounces off downward.
+                }
+                else if (ballDirection == 2)
+                {
+                    ballDirection = 4; //From upward right direction the ball bounces off downward right.
+                }
+                else if (ballDirection == 1)
+                {
+                    ballDirection = 5; //From upward right direction the ball bounces off downward right.
+                }
+            }
+
+            if (ballPositionX == 0) //When the ball hits the left wall.
+            {
+                if (ballDirection == 1)
+                {
+                    ballDirection = 2; //From upward left direction the ball bounces off upward right.
+                }
+                else if (ballDirection == 5)
+                {
+                    ballDirection = 4; //From downward left direction the ball bounces off downward right.
+                }
+            }
+
+            if (ballPositionX == PlaygroundWidth - 1) //When the ball hits the right wall.
+            {
+                if (ballDirection == 2)
+                {
+                    ballDirection = 1; //From upward right direction the ball bounces off upward left.
+                }
+                else if (ballDirection == 4)
+                {
+                    ballDirection = 5; //From downward right direction the ball bounces off downward left.
+                }
+            }
+        }
+        
+        private static void ChangePaddlePosition(ConsoleKeyInfo pressedKey)
         {
             if (pressedKey.Key == ConsoleKey.LeftArrow)
             {
-                playerPositionX = playerPositionX - 2;
-                if (playerPositionX < 0)
+                paddlePositionX = paddlePositionX - 2;
+                if (paddlePositionX < 0)
                 {
-                    playerPositionX = 0;
+                    paddlePositionX = 0;
                 }
             }
             else if (pressedKey.Key == ConsoleKey.RightArrow)
             {
-                playerPositionX = playerPositionX + 2;
-                if (playerPositionX > PlaygroundWidth - PaddleWidth - 1)
+                paddlePositionX = paddlePositionX + 2;
+                if (paddlePositionX > PlaygroundWidth - PaddleWidth - 1)
                 {
-                    playerPositionX = PlaygroundWidth - PaddleWidth - 1;
+                    paddlePositionX = PlaygroundWidth - PaddleWidth - 1;
                 }
             }
-
-            return playerPositionX;
         }
 
-        private static void DrawPaddle(int playerPositionX)
+        private static void DrawPaddle()
         {
             for (int i = 0; i < PaddleWidth; i++)
             {
-                Console.SetCursorPosition(playerPositionX + i, PlaygroundHeight - 1);
+                Console.SetCursorPosition(paddlePositionX + i, PlaygroundHeight - 1);
                 Console.Write('_');
             }
         }
 
-        private static void DrawBall(int ballPositionX, int ballPositionY)
+        private static void DrawBall()
         {
             Console.SetCursorPosition(ballPositionX, ballPositionY);
             Console.Write(BallSymbol);
