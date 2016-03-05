5761a72f2973878f144394f4e3dbb2c099dc5610
radi81 <rd_dimitrova@abv.bg>
2016-03-05 18:45:15 +0200

    clas Gmer + IGamer

---
+++ Breakout/Gamer.cs
@@ -0,0 +1,78 @@
+﻿namespace Breakout
+{
+    using System;
+
+    using Breakout.Contracts;
+
+    public class Gamer : IGamer
+    {
+        private string gamerName;
+
+        private bool gamerStartsTheGame = true;
+
+        private bool gamerEndsTheGameSuccessfully = false;
+
+        public Gamer()
+        {
+            this.GamerStartsTheGame = this.gamerStartsTheGame;
+            this.GamerEndsTheGameSuccessfully = this.gamerEndsTheGameSuccessfully;
+        }
+
+        public Gamer(string gamerName, int gamerPoints)
+        {
+            this.GamerName = gamerName;
+            this.GamerPoints = gamerPoints;
+        }
+
+        public string GamerName
+        {
+            get
+            {
+                return this.gamerName;
+            }
+
+            set 
+            {
+                ValidateGamerName(value);
+
+                this.gamerName = value;
+            }
+        }
+
+        public int GamerPoints
+        {
+            get 
+            {
+                return this.GamerPoints;
+            }
+
+            private set 
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+
+                this.GamerPoints = value;
+            }
+        }
+
+        public bool GamerStartsTheGame { get; private set; }
+
+        public bool GamerEndsTheGameSuccessfully { get; private set; }
+
+        private static void ValidateGamerName(string value)
+        {
+            while (true)
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    Console.Write("Please enter correct name: ");
+                    continue;
+                }
+                
+                break;
+            }
+        }
+    }
+}
