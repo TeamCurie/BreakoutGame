6c09bcf8fa3fcf842f1d993d03d665ee42467ce8
radi81 <rd_dimitrova@abv.bg>
2016-03-05 19:48:33 +0200

    Class Score in progress.

--- Breakout/Score.cs
+++ Breakout/Score.cs
@@ -1,6 +1,41 @@
 ﻿namespace Breakout
 {
+    using System;
+    using System.Collections.Generic;
+
+    using Breakout.Contracts;
+
     public class Score
     {
+        private const int TopGamersRanklistCount = 5;
+
+        private readonly List<IGamer> topGamersRanklist; 
+
+        private Gamer currentGamer;
+
+        public Score(Gamer gamer)
+        {
+            this.CurrentGamer = gamer;
+            this.topGamersRanklist = new List<IGamer>(TopGamersRanklistCount);
+        }
+        
+        public Gamer CurrentGamer { get; private set; }
+
+        private static void PrintHighScores(List<IGamer> gamerRatingList)
+        {
+            Console.WriteLine("Score: ");
+            if (gamerRatingList.Count > 0)
+            {
+                for (int i = 0; i < gamerRatingList.Count; i++)
+                {
+                    Console.WriteLine(
+                        $"{i + 1}.{gamerRatingList[i].GamerName} -> {gamerRatingList[i].GamerPoints} points");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Empty ranklist!");
+            }
+        }
     }
 }
