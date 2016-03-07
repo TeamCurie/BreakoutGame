56e75bef7cf584a196eb7716d68f57d9fbbcbc01
Teodora.Nozharova <tedinoji@gmail.com>
2016-03-07 21:47:21 +0200

    added IFillingPattern for implementing different types of walls

---
+++ Breakout/Models/Pattern.cs
@@ -0,0 +1,21 @@
+﻿namespace Breakout.Models
+{
+    using System;
+    using Contracts;
+
+    internal class Pattern
+    {
+        public void FillWall(IWall wall)
+        {
+            Console.SetCursorPosition(0, 1);
+
+            for (int row = 0; row < wall.Height; row++)
+            {
+                for (int column = 0; column < wall.Width; column++)
+                {
+                    wall.FilledWall[row, column] = new Brick(row, column);
+                }
+            }
+        }
+    }
+}
